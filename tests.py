8dc58a36b72abc4005ae0c152cb248c68640fd9e
kiriruyamato <kiriruyamato@gmail.com>
2016-10-30 22:17:15 +0300

    tests.py и правки в data_control

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+import sqlite3
+
+# Набор тестов, связанных с базой данных
+
+class databasetests(unittest.TestCase):
+
+    # Функция вводится, т.к. нельзя переопределить конструктор класса
+    def basefortest(self, datapath):
+        self.datapath = datapath
+
+    # Проверка на существование таблицы
+    def tabletest(self):
+        temporaryconnection = sqlite3.connect(self.datapath)
+        temporarycursor = temporaryconnection.cursor()
+        # Выполнение запроса вернет значение 0, если таблицы не существует
+        tempbool = temporarycursor.execute('SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'users' ')
+        self.assertTrue(tempbool)
